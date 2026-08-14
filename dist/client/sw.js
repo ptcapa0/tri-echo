4e175480260cafa11d969f80d4fe9d56970f6d6d
ptcapa0 <125266759+ptcapa0@users.noreply.github.com>
2026-08-14 16:16:11 +0100

    Fix middle pockets on long side cushions
    * Fix six-pocket side placement: js/generator.js
    
    * Fix six-pocket side placement: tests/core.test.js
    
    * Fix six-pocket side placement: package.json
    
    * Fix six-pocket side placement: sw.js
    
    * Fix six-pocket side placement: dist/client/js/generator.js
    
    * Fix six-pocket side placement: dist/client/sw.js

--- dist/client/sw.js
+++ dist/client/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tri-echo-v4.0.0';
+const CACHE='tri-echo-v4.0.1';
 const FILES=[
  './','./index.html','./style.css','./manifest.webmanifest',
  './assets/icon.svg','./assets/icon-192.png','./assets/icon-512.png',
